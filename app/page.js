ea2e7f3d38a9dee0be42b784f2ae5e1e4ba9c627
Jayveer-code <jayuchavda444@gmail.com>
2024-06-06 16:25:47 -0700

    Practice Set urls Routs...

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '@/Components/Header';
+
+const page = () => {
+  return (
+    <div className="container">
+      <><Header/></>
+      
+    </div>
+  );
+};
+
+export default page;
